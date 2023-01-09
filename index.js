d99882b2c7cb8100cc39febda031d0931b33e310
Gerard Busacco <gerardbusacco@gmail.com>
2023-01-09 15:08:59 -0500

    my fork

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+// Code your solution in this file!
+const companyName = 'Scuber';
+
+
+'mostProfitableNeighborhood' === 'Chelsea'
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+'companyCeo' === 'Susan Smith'
+
+let companyCeo = 'Susan Smith'
